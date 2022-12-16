7079ea16c48a7f27a7ac226bcdbaca8adb35b9e6
Thomas Staud <thstaud@tsn.at>
2022-12-16 17:46:08 +0100

    parameter matching test

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
 
-    println!("In file {}", file_path);
-
     let script = fs::read_to_string(file_path)
         .expect("Invalid File Path");
     let commands = script.split(".");
 
     for command in commands {
         println!("{}", command.trim());
+        let mut params = command.split(" ").into_iter();
+
+        match params.next() {
+            Some("Wabe") => println!("Wambe command"),
+            Some("Biene") => println!("Biene command"),
+            _ => println!("Unknown Command exception")
+        }
     }
 }
