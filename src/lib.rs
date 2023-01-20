0de87c6a9f75734c9c899ba33f76b5b7c38fbae2
Thomas Staud <thstaud@tsn.at>
2023-01-20 21:11:07 +0100

    sammeln & bringen added
    sammeln = getting input from user
    bringen = saving input in a wabe

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,156 @@
-use core::time;
-use std::thread;
+use std::{thread, time::Duration, sync::{Mutex, Arc}, io};
+
+struct Biene {
+    name: String,
+    nektar: u32
+}
+
+struct Wabe {
+    name: String,
+    honig: u32
+}
 
 // executes bienenstock code
 pub fn run(commands: Vec<&str>) {
-    let mut biene_handles = vec![];
+    let sleep_secs = 1;
 
-    let mut biene_busy = [false; 1000];
-    let mut wabe_names = [""; 1000];
-    let mut wabe_honig = [0; 1000];
+    let mut biene_handles = vec![];
+    let biene_busy = Arc::new(Mutex::new([false; 1000]));
+
+    // generate 1000 bienen
+    let bienen = Arc::new(Mutex::new(vec![]));
+    for _ in 0..1000 {
+        bienen.lock().unwrap().push(Biene {
+            name: String::new(),
+            nektar: 0
+        })
+    }
+
+    // generate 1000 waben
+    let waben = Arc::new(Mutex::new(vec![]));
+    for _ in 0..1000 {
+        waben.lock().unwrap().push(Wabe {
+            name: String::new(),
+            honig: 0
+        })
+    }
 
     for command in commands {
         let mut params = command.split(" ");
         // execute code based on command
-        match params.next() {
-            Some("Wabe") => {
+        match params.next().unwrap() {
+            "Wabe" => {
                 // rename wabe
-                let wabe_index: usize = params.next().expect("Syntax Error")
-                    .parse().expect("Syntax Error");
-                let wabe_name: &str = command.split("\"").nth(1)
-                    .expect("Syntax Error");
-                wabe_names[wabe_index] = wabe_name;
-            }
-            Some("Biene") => {
-                // tanze name of wabe
-                let biene_index: usize = params.next().expect("Syntax Error")
-                    .split(",").next().expect("Syntax Error")
-                    .parse().expect("Syntax Error");
+                let wabe_index: usize = params.next().unwrap().parse().unwrap();
+                let wabe_name: &str = command.split("\"").nth(1).unwrap();
+                waben.lock().unwrap()[wabe_index].name = wabe_name.to_string();
+            },
+            "Biene" => {
+                // find biene index
+                let biene_index: usize = params
+                    .next()
+                    .unwrap()
+                    .split(",")
+                    .next()
+                    .unwrap()
+                    .parse::<usize>()
+                    .unwrap()
+                    .clone();
 
-                if biene_busy[biene_index] {
+                if biene_busy.lock().unwrap()[biene_index] {
                     // crash if biene is already doing something
                     panic!("Biene {biene_index} is busy!");
                 } else {
-                    if params.next() != Some("tanze") { panic!("Syntax Error"); }
-                    if params.next() != Some("den") { panic!("Syntax Error"); }
-                    if params.next() != Some("Namen") { panic!("Syntax Error"); }
-                    if params.next() != Some("von") { panic!("Syntax Error"); }
-                    if params.next() != Some("Wabe") { panic!("Syntax Error"); }
-
-                    let wabe_index: usize = params.next().expect("Syntax Error")
-                        .parse().expect("Syntax Error");
-                    let wabe_name = String::from(wabe_names[wabe_index]);
-
-                    // spawn thread for biene to tanz
-                    let handle = thread::spawn(move || {
-                        thread::sleep(time::Duration::from_secs(2));
-                        println!("{wabe_name}");
-                    });
-                    biene_handles.push(handle);
+                    // set biene to busy
+                    biene_busy.lock().unwrap()[biene_index] = true;
                 }
 
+                match params.next().unwrap() {
+                    "tanze" => {
+                        // check what should be printed
+                        let name: bool = match params.nth(1) {
+                            Some("Namen") => true,
+                            Some("Honig") => false,
+                            _ => panic!("Syntax Error"),
+                        };
+
+                        let wabe_index: usize = params.nth(2).unwrap().parse().unwrap();
+
+                        // store whatever should be printed
+                        let print = match name {
+                            true => waben.lock().unwrap()[wabe_index].name.to_owned(),
+                            false => waben.lock().unwrap()[wabe_index].honig.to_string(),
+                        };
+
+                        // spawn thread for biene to tanz
+                        let biene_busy = Arc::clone(&biene_busy);
+                        let handle = thread::spawn(move || {
+                            thread::sleep(Duration::from_secs(sleep_secs));
+                            println!("{print}");
+                            biene_busy.lock().unwrap()[biene_index] = false;
+                        });
+                        biene_handles.push(handle);
+                    }
+                    "sammle" => {
+                        // sammle input from user
+                        let mut input = String::new();
+                        io::stdin().read_line(&mut input).unwrap();
+
+                        let name: bool = match params.next() {
+                            Some("Namen") => true,
+                            Some("Nektar") => false,
+                            _ => panic!("Syntax Error"),
+                        };
+
+                        // spawn thread for biene to sammel
+                        let biene_busy = Arc::clone(&biene_busy);
+                        let bienen = Arc::clone(&bienen);
+                        let handle = thread::spawn(move || {
+                            thread::sleep(Duration::from_secs(sleep_secs));
+                            match name {
+                                true => bienen.lock().unwrap()[biene_index].name = input.trim().to_string(),
+                                false => bienen.lock().unwrap()[biene_index].nektar = input.trim().parse().unwrap()
+                            };
+                            biene_busy.lock().unwrap()[biene_index] = false;
+                        });
+                        biene_handles.push(handle);
+                    }
+                    "bringe" => {
+                        // save name or nektar to wabe
+                        let name: bool = match params.next() {
+                            Some("Namen") => true,
+                            Some("Nektar") => false,
+                            _ => panic!("Syntax Error"),
+                        };
+
+                        let wabe_index: usize = params.nth(2).unwrap().parse().unwrap();
+                        
+                        // spawn thread for biene to bring
+                        let biene_busy = Arc::clone(&biene_busy);
+                        let bienen = Arc::clone(&bienen);
+                        let waben = Arc::clone(&waben);
+                        let handle = thread::spawn(move || {
+                            thread::sleep(Duration::from_secs(sleep_secs));
+                            match name {
+                                true => waben.lock().unwrap()[wabe_index].name = bienen.lock().unwrap()[biene_index].name.to_owned(),
+                                false => waben.lock().unwrap()[wabe_index].honig += bienen.lock().unwrap()[biene_index].nektar
+                            };
+                            biene_busy.lock().unwrap()[biene_index] = false;
+                        });
+                        biene_handles.push(handle);
+                    }
+                    _ => panic!("Unknown Command: '{}'", command),
+                }
             }
-            Some("So!") => println!("So!"),
-            Some(param) => panic!("Unknown Command: '{}'", param),
-            _ => ()
+            "Warte" => {
+                // wait for thread to end
+                let biene_index: usize = params.nth(2).unwrap().parse().unwrap();
+                while biene_busy.lock().unwrap()[biene_index] {
+                    thread::sleep(Duration::from_millis(1));
+                }
+            }
+            "So!" => (),
+            _ => panic!("Unknown Command: '{}'", command),
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use std::{env, fs};
+use std::{env, fs, time::Instant};
 
 use bienenstock::run;
 
 fn main() {
+    let now = Instant::now();
+
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
 
-    let script = fs::read_to_string(file_path)
-        .expect("Invalid File Path");
+    let script = fs::read_to_string(file_path).expect("Invalid File Path");
     let commands = script.split(".\r\n").collect();
 
     run(commands);
+
+    let elapsed = now.elapsed();
+    println!("Bienenstock ist fertig. Ausführdauer: {:.2?}", elapsed);
 }
